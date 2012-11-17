38db50daea37f4ca745f5e790f8c230e541d0c90
Ziqi Fan <ziqifan16@gmail.com>
2012-11-17 11:23:32 -0600

    second version of checkrule

--- src/edu/umn/se/trap/CheckRuleDODFund.java
+++ src/edu/umn/se/trap/CheckRuleDODFund.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Map;
 import edu.umn.se.*;
 import edu.umn.se.trap.db.GrantDB;
+import edu.umn.se.trap.db.GrantDB.GRANT_FIELDS;
+import edu.umn.se.trap.db.KeyNotFoundException;
 import edu.umn.se.trap.db.UserGrantDB;
 
 public class CheckRuleDODFund {
@@ -13,9 +15,20 @@ public class CheckRuleDODFund {
 	private Map<String, String> formData;
 	private List<String> userGrantInfo;
 	private List<Object> grantInfo;
-	public void checkRuleDODFund(int formId) throws Exception {
+	private int i=0;
+	public void checkRuleDODFund(int formId) throws KeyNotFoundException {
 		formData = intf.getSavedFormData(formId);
-		userGrantInfo = userGrantDB.getUserGrantInfo(formData.get(GRANT1_ACCOUNT));
-		grantInfo = grantDB.getGrantInfo(formData.get(GRANT1_ACCOUNT));
+		for(i=0;i<formData.get(NUM_GRANTS);i++) {
+			userGrantInfo = userGrantDB.getUserGrantInfo(formData.get(GRANT1_ACCOUNT));
+			if(userGrantInfo.contains(formData.get(USER_NAME)==false){
+				throw new KeyNotFoundException("User " + formData.get(USER_NAME) +" is not supported by" +
+						" account number "+formData.get(GRANT1_ACCOUNT) + ".\n");
+			}
+			grantInfo = grantDB.getGrantInfo(formData.get(GRANT1_ACCOUNT));
+			if(grantInfo.get(GRANT_FIELDS.ACCOUNT_TYPE.ordinal())=="DOD"){
+				
+			}
+		}
+		
 	}
 }
